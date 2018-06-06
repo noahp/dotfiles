a4f145bb13098d556bb73b668f84e9e6222730a7
noahp <2538614+noahp@users.noreply.github.com>
2018-06-06 11:38:48 -0400

    Closes #1- Switch to dotbot deploy
    TODO- remainder of setup (spaceship theme + proper oh-my-zsh, rg + fd)

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# deploy dotfiles with stow
-./stowsh/stowsh gdb -t ~
-./stowsh/stowsh tmux -t ~
-./stowsh/stowsh vim -t ~
-./stowsh/stowsh zsh -t ~
-./stowsh/stowsh urlview -t ~
-./stowsh/stowsh vscode -t ~/.config/Code/User
--- undeploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# undeploy dotfiles with stow
-./stowsh/stowsh -D gdb -t ~
-./stowsh/stowsh -D tmux -t ~
-./stowsh/stowsh -D vim -t ~
-./stowsh/stowsh -D zsh -t ~
-./stowsh/stowsh -D vscode -t ~/.config/Code/User
-./stowsh/stowsh -D urlview -t ~
