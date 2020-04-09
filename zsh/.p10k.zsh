a6f5e382a3fab14639aa38f3241d163a6e88031a
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2020-04-09 09:40:18 -0400

    ⌚ UTC/locale date and time
    Some updates to reduce redundancy and confusion around dates + times.
    
    - switch to ISO-8601 date and time in tmux status bar (local time
    displayed in gnome already)
    - add TZ string to powerlevel10k time
    
    References:
    
    - https://man.openbsd.org/strftime.3
    - https://man.openbsd.org/tmux#FORMATS

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -789,7 +789,7 @@
   # Current time color.
   typeset -g POWERLEVEL9K_TIME_FOREGROUND=66
   # Format for the current time: 09:51:02. See `man 3 strftime`.
-  typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S}'
+  typeset -g POWERLEVEL9K_TIME_FORMAT='%D{%H:%M:%S %Z}'
   # If set to true, time will update when you hit enter. This way prompts for the past
   # commands will contain the start times of their commands as opposed to the default
   # behavior where they contain the end times of their preceding commands.
