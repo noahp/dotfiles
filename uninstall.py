d33f268a4b4abe298f87c56d5e531d2c4d204560
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2022-10-24 11:24:09 -0400

    run pre-commit

--- uninstall.py
+++ uninstall.py
@@ -6,17 +6,18 @@ https://github.com/anishathalye/dotbot/issues/152#issuecomment-394129600
 """
 from __future__ import print_function
 
-import yaml
 import os
 
-CONFIG="install.conf.yaml"
+import yaml
+
+CONFIG = "install.conf.yaml"
 
 stream = open(CONFIG, "r")
 conf = yaml.load(stream)
 
 for section in conf:
-    if 'link' in section:
-        for target in section['link']:
+    if "link" in section:
+        for target in section["link"]:
             realpath = os.path.expanduser(target)
             if os.path.islink(realpath):
                 print("Removing ", realpath)
